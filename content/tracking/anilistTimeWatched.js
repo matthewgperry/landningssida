9423f18fbf28a6365e7256160060f3577f0b1b0b
Matthew Perry <mattp@tuta.io>
2022-03-27 16:08:00 -0400

    Added anime tracking page, with auto updating stats

---
+++ content/tracking/anilistTimeWatched.js
@@ -0,0 +1,54 @@
+var query = `query {
+  User(id: 5313378) {
+    statistics {
+      anime {
+        minutesWatched
+        episodesWatched
+        count
+      }
+    }
+  }
+  MediaList(userId: 5313378, sort: UPDATED_TIME_DESC) {
+    media {
+      title {
+        english
+      }
+    }
+  }
+}`;
+
+var url = 'https://graphql.anilist.co',
+    options = {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            query: query
+        })
+    };
+
+fetch(url, options).then(handleResponse)
+                    .then(handleData)
+                    .catch(handleError);
+
+function handleResponse(response) {
+    return response.json().then(function (json) {
+        return response.ok ? json : Promise.reject(json);
+    });
+}
+
+function handleData(data) {
+  var minutesWatched = data["data"]["User"]["statistics"]["anime"]["minutesWatched"];
+  var episodesWatched = data["data"]["User"]["statistics"]["anime"]["episodesWatched"];
+  var lastWatched = data["data"]["MediaList"]["media"]["title"]["english"];
+  document.getElementById("minutesWatched").innerHTML = minutesWatched;
+  document.getElementById("episodesWatched").innerHTML = episodesWatched;
+  document.getElementById("lastWatched").innerHTML = lastWatched;
+}
+
+function handleError(error) {
+    alert('Error, check console');
+    console.error(error);
+}
---
+++ content/tracking/animeTracking.md
@@ -0,0 +1,17 @@
++++
+title = "Tracking Anime"
+template = "anime.html"
++++
+
+## Watching and Tracking Anime Series
+
+###### This page uses javascript to update the stats.
+<script src="../anilistTimeWatched.js"></script>
+<b id="minutesWatched"></b>
+Minutes Watched
+
+<b id="episodesWatched"></b>
+Total Episodes Watched
+
+Last Series Watched:
+<b id="lastWatched"></b>
